94871ce5566c05d8495a7c504a5b7c0b70f9f491
Jason <jmdrews2@illinois.edu>
2019-10-23 23:02:57 -0500

    Small tweaks and magic button

--- assets/js/api_test.js
+++ assets/js/api_test.js
@@ -13,19 +13,18 @@ request.onload = function() {
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400) {
     data.forEach(jq => {
-      console.log(jq.id)
 
       const qcard = document.createElement('div')
       qcard.setAttribute('class', 'qcard')
 
-      const h1 = document.createElement('h4')
-      h1.textContent = jq.question
+      const h4 = document.createElement('h4')
+      h4.textContent = jq.question
 
       const p = document.createElement('p')
       p.textContent = jq.answer
 
       container.appendChild(qcard)
-      qcard.appendChild(h1)
+      qcard.appendChild(h4)
       qcard.appendChild(p)
     })
   } else {
