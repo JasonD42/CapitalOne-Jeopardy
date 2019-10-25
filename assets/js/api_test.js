2de60270b89c5978a31935cb408004817933d9ae
Jason <jmdrews2@illinois.edu>
2019-10-25 14:41:31 -0500

    Api call debugging

--- assets/js/api_test.js
+++ assets/js/api_test.js
@@ -1,129 +1,200 @@
-let elements = document.getElementsByTagName('randq1');
+//Q1
+var app = document.getElementById('randq1');
 
-for (let app of elements) {
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
 
-  const container = document.createElement('div')
-  container.setAttribute('class', 'container')
+app.appendChild(container)
 
-  app.appendChild(container)
+var request = new XMLHttpRequest()
+// cors https://cors-anywhere.herokuapp.com/
+request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
+request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  console.log(request.status)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(jq => {
 
-  var request = new XMLHttpRequest()
-  // cors https://cors-anywhere.herokuapp.com/
-  request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  request.onload = function() {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-    console.log(request.status)
-    if (request.status >= 200 && request.status < 400) {
-      data.forEach(jq => {
+      const qcard = document.createElement('div')
+      qcard.setAttribute('class', 'qcard')
 
-        const qcard = document.createElement('div')
-        qcard.setAttribute('class', 'qcard')
+      const h4 = document.createElement('h4')
+      h4.textContent = jq.question
 
-        const h4 = document.createElement('h4')
-        h4.textContent = jq.question
+      const p = document.createElement('p')
+      p.textContent = jq.answer
 
-        const p = document.createElement('p')
-        p.textContent = jq.answer
-
-        container.appendChild(qcard)
-        qcard.appendChild(h4)
-        qcard.appendChild(p)
-      })
-    } else {
-      const errorMessage = document.createElement('marquee')
-      errorMessage.textContent = `Gah, it's not working!`
-      app.appendChild(errorMessage)
-    }
+      container.appendChild(qcard)
+      qcard.appendChild(h4)
+      qcard.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
   }
+}
+request.send()
 
-  request.send()
+//Q2
+app = document.getElementById('randq2')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+// cors https://cors-anywhere.herokuapp.com/
+request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
+request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  console.log(request.status)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(jq => {
+
+      const qcard = document.createElement('div')
+      qcard.setAttribute('class', 'qcard')
+
+      const h4 = document.createElement('h4')
+      h4.textContent = jq.question
+
+      const p = document.createElement('p')
+      p.textContent = jq.answer
+
+      container.appendChild(qcard)
+      qcard.appendChild(h4)
+      qcard.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
+}
+request.send()
+
+//Q3
+app = document.getElementById('randq3')
+
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
+
+app.appendChild(container)
+
+var request = new XMLHttpRequest()
+// cors https://cors-anywhere.herokuapp.com/
+request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
+request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  console.log(request.status)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(jq => {
+
+      const qcard = document.createElement('div')
+      qcard.setAttribute('class', 'qcard')
+
+      const h4 = document.createElement('h4')
+      h4.textContent = jq.question
+
+      const p = document.createElement('p')
+      p.textContent = jq.answer
+
+      container.appendChild(qcard)
+      qcard.appendChild(h4)
+      qcard.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
+  }
 }
 
-let elements2 = document.getElementsByTagName('randq2');
+request.send()
 
-for (let app of elements) {
+//Q4
+app = document.getElementById('randq4')
 
-  const container = document.createElement('div')
-  container.setAttribute('class', 'container')
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
 
-  app.appendChild(container)
+app.appendChild(container)
 
-  var request = new XMLHttpRequest()
-  // cors https://cors-anywhere.herokuapp.com/
-  request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  request.onload = function() {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-    console.log(request.status)
-    if (request.status >= 200 && request.status < 400) {
-      data.forEach(jq => {
+var request = new XMLHttpRequest()
+// cors https://cors-anywhere.herokuapp.com/
+request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
+request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  console.log(request.status)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(jq => {
 
-        const qcard = document.createElement('div')
-        qcard.setAttribute('class', 'qcard')
+      const qcard = document.createElement('div')
+      qcard.setAttribute('class', 'qcard')
 
-        const h4 = document.createElement('h4')
-        h4.textContent = jq.question
+      const h4 = document.createElement('h4')
+      h4.textContent = jq.question
 
-        const p = document.createElement('p')
-        p.textContent = jq.answer
+      const p = document.createElement('p')
+      p.textContent = jq.answer
 
-        container.appendChild(qcard)
-        qcard.appendChild(h4)
-        qcard.appendChild(p)
-      })
-    } else {
-      const errorMessage = document.createElement('marquee')
-      errorMessage.textContent = `Gah, it's not working!`
-      app.appendChild(errorMessage)
-    }
+      container.appendChild(qcard)
+      qcard.appendChild(h4)
+      qcard.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
   }
-
-  request.send()
 }
+request.send()
 
-let elements3 = document.getElementsByTagName('randq3');
+// Q5
+app = document.getElementById('randq5')
 
-for (let app of elements3) {
+const container = document.createElement('div')
+container.setAttribute('class', 'container')
 
-  const container = document.createElement('div')
-  container.setAttribute('class', 'container')
+app.appendChild(container)
 
-  app.appendChild(container)
+var request = new XMLHttpRequest()
+// cors https://cors-anywhere.herokuapp.com/
+request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
+request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+request.onload = function () {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response)
+  console.log(request.status)
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(jq => {
 
-  var request = new XMLHttpRequest()
-  // cors https://cors-anywhere.herokuapp.com/
-  request.open('GET', 'https://cors-anywhere.herokuapp.com/http://jservice.io/api/random', true)
-  request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-  request.onload = function() {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-    console.log(request.status)
-    if (request.status >= 200 && request.status < 400) {
-      data.forEach(jq => {
+      const qcard = document.createElement('div')
+      qcard.setAttribute('class', 'qcard')
 
-        const qcard = document.createElement('div')
-        qcard.setAttribute('class', 'qcard')
+      const h4 = document.createElement('h4')
+      h4.textContent = jq.question
 
-        const h4 = document.createElement('h4')
-        h4.textContent = jq.question
+      const p = document.createElement('p')
+      p.textContent = jq.answer
 
-        const p = document.createElement('p')
-        p.textContent = jq.answer
-
-        container.appendChild(qcard)
-        qcard.appendChild(h4)
-        qcard.appendChild(p)
-      })
-    } else {
-      const errorMessage = document.createElement('marquee')
-      errorMessage.textContent = `Gah, it's not working!`
-      app.appendChild(errorMessage)
-    }
+      container.appendChild(qcard)
+      qcard.appendChild(h4)
+      qcard.appendChild(p)
+    })
+  } else {
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Gah, it's not working!`
+    app.appendChild(errorMessage)
   }
-
-  request.send()
 }
-
+request.send()
